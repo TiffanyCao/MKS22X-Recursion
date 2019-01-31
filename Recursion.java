02cf0a7757d6e562be3546625401a5c88d4ce9b6
TiffanyCao <tcao00@stuy.edu>
2019-01-31 15:09:07 -0800

    added fibonacci method and test cases for sqrt

--- Recursion.java
+++ Recursion.java
@@ -11,4 +11,31 @@ public class Recursion{
       return sqrtH(n, (n / guess + guess) / 2, tolerance);
     }
   }
+
+  public static int fib(int n){
+    return fibH(n, 0, 1, 0);
+  }
+
+  public static int fibH(int n, int first, int second, int count){
+    if(count == n){
+      return first;
+    }else{
+      return fibH(n, second, first + second, count + 1);
+    }
+  }
+
+  public static void main(String[] args){
+
+    System.out.println("---Testing Sqrt---");
+    System.out.println("*testing sqrt(100): should return 10*");
+    System.out.println(sqrt(100, 0.001));
+    System.out.println("*testing sqrt(25): should return 5*");
+    System.out.println(sqrt(25, 0.001));
+    System.out.println("*testing sqrt(16): should return 4*");
+    System.out.println(sqrt(16, 0.001));
+    System.out.println("*testing sqrt(169): should return 13*");
+    System.out.println(sqrt(169, 0.001));
+    System.out.println("*testing sqrt(2): should return 1.41421*");
+    System.out.println(sqrt(2, 0.001));
+  }
 }
